026ce7d6a3475e7ec58413cc02492ad56e6dd600
Brendan Murty <brendan@murty.au>
2024-07-25 14:27:02 +1000

    Add new TypeScript solution from July 2024

---
+++ typescript/calculator-with-method-chaining/README.md
@@ -0,0 +1,1 @@
+[Link to problem](https://leetcode.com/problems/calculator-with-method-chaining/description/)
---
+++ typescript/calculator-with-method-chaining/calculator-with-method-chaining.ts
@@ -0,0 +1,44 @@
+type CalculatorObject = {
+  value: number;
+}
+
+class Calculator {
+  value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+  
+  add(value: number): CalculatorObject {
+    this.value += value;
+    return this;
+  }
+  
+  subtract(value: number): CalculatorObject {
+    this.value -= value;
+    return this;
+  }
+  
+  multiply(value: number): CalculatorObject {
+    this.value *= value;
+    return this;
+  }
+  
+  divide(value: number): CalculatorObject {
+    if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    }
+
+    this.value /= value;
+    return this;
+  }
+  
+  power(value: number): CalculatorObject {
+    this.value **= value;
+    return this;
+  }
+  
+  getResult(): number {
+    return this.value;
+  }
+}
