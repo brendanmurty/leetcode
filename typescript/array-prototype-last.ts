c7434af3e4f9e32c8c7018692c4dd8551bc85cd1
Brendan Murty <brendan@murty.au>
2024-07-25 14:49:34 +1000

    Add new TypeScript solution from July 2024

---
+++ typescript/array-prototype-last.ts
@@ -0,0 +1,13 @@
+// Problem: https://leetcode.com/problems/array-prototype-last/description/
+
+interface Array<T> {
+  last(): T | -1;
+}
+
+Array.prototype.last = function() {
+  if (this.length === 0) {
+    return -1;
+  }
+
+  return this[this.length-1];
+};
