95c90c274a98d7e6412ad80cffdf74ecced30076
Brendan Murty <brendan@murty.au>
2024-07-25 15:00:49 +1000

    Add new TypeScript solution from July 2024

---
+++ typescript/sort-by.ts
@@ -0,0 +1,9 @@
+// Problem: https://leetcode.com/problems/sort-by/description/
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (value: JSONValue) => number
+
+function sortBy(arr: JSONValue[], fn: Fn): JSONValue[] {
+  let arrSorted: JSONValue[] = arr.sort((x, y) => fn(x) - fn(y));
+  return arrSorted;
+};
